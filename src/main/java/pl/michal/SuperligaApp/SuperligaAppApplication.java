d5288b62ac9908274e7ffbf00ba1089cc4821225
mbull <mbullanda@gmail.com>
2021-06-05 10:08:52 +0200

    initialize project

---
+++ src/main/java/pl/michal/SuperligaApp/SuperligaAppApplication.java
@@ -0,0 +1,13 @@
+package pl.michal.SuperligaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperligaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperligaAppApplication.class, args);
+	}
+
+}
